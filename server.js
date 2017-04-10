66c6e21da7b25ef4e57a999e30dbe5578d8dab90
Jiayu Liu <jiayu@madadata.com>
2017-04-10 13:07:32 +0800

    dev server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // @flow
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.conf')
 const { graphqlExpress } = require('graphql-server-express')
 const schema = require('./data')
 
@@ -11,6 +15,19 @@ const app = express()
 app.use(bodyParser.json())
 app.use('/graphql', graphqlExpress({ schema }))
 
+app.use(webpackDevMiddleware(webpack(webpackConfig), {
+  noInfo: false,
+  quiet: false,
+  publicPath: '/static/',
+  stats: {
+    colors: true
+  }
+}))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
 app.listen(PORT, '0.0.0.0', () => {
-  console.info(`server started at port ${PORT}`)
+  console.info(`dev server started at port ${PORT}`)
 })
