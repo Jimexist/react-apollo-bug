59e0305405d4b8d61e7b51d8508eb75b8d1489c2
Jiayu Liu <jiayu@madadata.com>
2017-04-10 13:00:40 +0800

    webpack

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,15 @@
 // @flow
 import React from 'react'
-import { gql, graphql } from 'react-apollo'
 import PropTypes from 'prop-types'
 
 class App extends React.Component {
-  static propTypes = {
-  }
-
   render () {
-
+    return <div>app</div>
   }
+}
+
+App.propTypes = {
+  children: PropTypes.array
 }
 
 export default App
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,5 +1,25 @@
 const path = require('path')
 
 module.exports = {
-
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 }
